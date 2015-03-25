732e1e3eaaacae0cacf23f3fead19a340853168c
Matthew Barnes <matthewearlbarnes@gmail.com>
2015-03-25 10:49:26 -0500

    Tile class
    Added the code for a basic tile

--- Gambit/src/com/lasacsgames/game/level/tile/Tile.java
+++ Gambit/src/com/lasacsgames/game/level/tile/Tile.java
@@ -1,8 +1,23 @@
 package com.lasacsgames.game.level.tile;
 
+import com.lasacsgames.game.graphics.Screen;
 import com.lasacsgames.game.graphics.Sprite;
 
 public abstract class Tile {
 
+	public int x, y;
 	public Sprite sprite;
+	
+	public Tile(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	public void render(int x, int y, Screen screen) {
+
+	}
+
+	public boolean solid() {
+		return false;
+	}
+
 }
