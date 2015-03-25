c9fc5db888f70e1a8a82e1e68c28cac78688397a
Matthew Barnes <matthewearlbarnes@gmail.com>
2015-03-25 14:04:25 -0500

    Fixed a tile render problem
    Also changed the color of void tiles.

--- Gambit/src/com/lasacsgames/game/graphics/Sprite.java
+++ Gambit/src/com/lasacsgames/game/graphics/Sprite.java
@@ -8,7 +8,7 @@ public class Sprite {
 	private SpriteSheet sheet;
 
 	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
-	public static Sprite voidSprite = new Sprite(16, 0x222222);
+	public static Sprite voidSprite = new Sprite(16, 0x87CEEB);
 	public static Sprite flower = new Sprite(16, 1, 0, SpriteSheet.tiles);
 	public static Sprite rock = new Sprite(16, 2, 0, SpriteSheet.tiles);
 
--- Gambit/src/com/lasacsgames/game/level/tile/VoidTile.java
+++ Gambit/src/com/lasacsgames/game/level/tile/VoidTile.java
@@ -1,14 +1,23 @@
 package com.lasacsgames.game.level.tile;
 
+import com.lasacsgames.game.graphics.Screen;
 import com.lasacsgames.game.graphics.Sprite;
 
-public class VoidTile extends Tile {
+public class VoidTile extends Tile
+{
 
-	public VoidTile(Sprite sprite) {
+	public VoidTile(Sprite sprite)
+	{
 		super(sprite);
 	}
 
-	public boolean solid() {
+	public void render(int x, int y, Screen screen)
+	{
+		screen.renderTile(x << 4, y << 4, this);
+	}
+
+	public boolean solid()
+	{
 		return true;
 	}
 
