e9fb0af3f439f3a2158474832741ca3fb30966f5
XeroOl <xerool6@gmail.com>
2015-03-30 11:29:14 -0500

    RandomLevel
    Changed things

--- Gambit/src/com/lasacsgames/game/level/Level.java
+++ Gambit/src/com/lasacsgames/game/level/Level.java
@@ -18,42 +18,44 @@ public abstract class Level {
 	}
 
 	public void setHeight(int height) {
-		this.height=height;
+		this.height = height;
 		tiles = new int[width][height];
 	}
-	public void setDimension(int width, int height){
+
+	public void setDimension(int width, int height) {
 		this.width = width;
-		this.height=height;
+		this.height = height;
 		tiles = new int[width][height];
 	}
-	
-	public void update()
-	{
+
+	public void update() {
 
 	}
 
-	public void render(int xScroll, int yScroll, Screen screen)
-	{
+	public void render(int xScroll, int yScroll, Screen screen) {
 		screen.setOffSet(xScroll, yScroll);
 		int x0 = xScroll >> 4;
 		int x1 = (xScroll + screen.width + 16) >> 4;
 		int y0 = yScroll >> 4;
 		int y1 = (yScroll + screen.height + 16) >> 4;
 
-		for (int y = y0; y < y1; y++)
-		{
-			for (int x = x0; x < x1; x++)
-			{
+		for (int y = y0; y < y1; y++) {
+			for (int x = x0; x < x1; x++) {
 				getTile(x, y).render(x, y, screen);
 			}
 		}
 
 	}
-
-	public Tile getTile(int x, int y)
-	{
-		if (x < 0 || y < 0 || x >= width || y >= height) return Tile.voidTile;
-		if (tiles[x][y] == 0xffffffff) return Tile.grass;
+	public int getTileId(int x, int y){
+		if (x < 0 || y < 0 || x >= width || y >= height)return 0x00000000;
+		else return tiles[x][y];
+		
+	}
+	public Tile getTile(int x, int y) {
+		if (x < 0 || y < 0 || x >= width || y >= height)
+			return Tile.voidTile;
+		if (tiles[x][y] == 0xffffffff)
+			return Tile.grass;
 		return Tile.voidTile;
 	}
 
--- Gambit/src/com/lasacsgames/game/level/RandomizedLevel.java
+++ Gambit/src/com/lasacsgames/game/level/RandomizedLevel.java
@@ -9,7 +9,7 @@ public class RandomizedLevel extends Level {
 		setDimension(width, height);
 		r = new Random();
 		generateLevel();
-		
+
 	}
 
 	public RandomizedLevel(int width, int height, int seed) {
@@ -19,32 +19,94 @@ public class RandomizedLevel extends Level {
 	}
 
 	protected void generateLevel() {
-		for(int x = 0; x<width;x++){
-			for(int y = 0; y<width; y++){
-				tiles[x][y]=0x00000000;
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < width; y++) {
+				tiles[x][y] = r.nextBoolean() ? 0x00000000 : 0xffffffff;
 			}
 		}
+
+		tiles = gol(9, 7);
+		tiles = gol(7, 5);
+		tiles = gol(9, 5);
+	}
+
+	protected int[][] gol(int numtosolid, int numtoair) {
+		int[][] newTiles = new int[width][height];
+		for (int x = 0; x < width; x++)
+			for (int y = 0; y < height; y++) {
+				newTiles[x][y] = tiles[x][y];
+				if (getTile(x, y).solid()) {
+					if (getNeighbors(x, y, 1, 0xffffffff) >= numtoair)
+						newTiles[x][y] = 0xffffffff;
+				} else {
+					if (getNeighbors(x, y, 1, 0x00000000) >= numtosolid)
+						newTiles[x][y] = 0x00000000;
+				}
+			}
+		return newTiles;
+	}
+
+	protected int getNeighbors(int x, int y, int dist, int countme) {
+		int count = 0;
+		for (int x1 = x - dist; x1 <= x + dist; x1++) {
+			for (int y1 = y - dist; y1 <= y + dist; y1++) {
+				if (x1 != x || y1 != y) {
+					if (getTileId(x1, y1) == countme) {
+						count++;
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	public void connect(int x1, int y1, int x2, int y2) {
+		int[][] weight = new int[width][height];
+		for(int x=0;x<width;x++){
+			for(int y=0;y<height;y++){
+				weight[x][y]=100000;
+			}
+		}
+		weight[x1][y1]=0;
+		process(x1, y1, weight);
+		followWeight(x2,y2);
+
+	}
+	protected void followWeight(int x, int y){
 		
-		// PLACEHOLDER GENERATOR, DO NOT ATTEMPT TO UNDERSTAND
-		int tilesleft = width * height / 2;
-		int x = r.nextInt(width);
-		int y = r.nextInt(height);
-		for (int i = 0; i < tilesleft; i++) {
-			tiles[x][y] = 0xffffffff;
-			int dir = r.nextInt(4);
-			if (dir == 0 && x != 0) {
-				x--;
+	}
+	protected void process(int x, int y, int[][] weight) {
+		if (x > 0) {
+			if (weight[x - 1][y] > weight[x][y]
+					+ (getTileId(x, y) == 0 ? 4 : 1)) {
+				weight[x - 1][y] = weight[x][y]
+						+ (getTileId(x, y) == 0 ? 4 : 1);
+				process(x - 1, y, weight);
 			}
-			if (dir == 1 && y != 0) {
-				y--;
+		}
+		if (y > 0) {
+			if (weight[x][y - 1] > weight[x][y]
+					+ (getTileId(x, y) == 0 ? 4 : 1)) {
+				weight[x][y - 1] = weight[x][y]
+						+ (getTileId(x, y) == 0 ? 4 : 1);
+				process(x, y - 1, weight);
 			}
-			if (dir == 2 && x < width - 1) {
-				x++;
+		}
+		if (x <width-1) {
+			if (weight[x +1][y] > weight[x][y]
+					+ (getTileId(x, y) == 0 ? 4 : 1)) {
+				weight[x + 1][y] = weight[x][y]
+						+ (getTileId(x, y) == 0 ? 4 : 1);
+				process(x + 1, y, weight);
 			}
-			if (dir == 3 && y < height - 1) {
-				y++;
+		}
+		if (y <height-1) {
+			if (weight[x ][y+1] > weight[x][y]
+					+ (getTileId(x, y) == 0 ? 4 : 1)) {
+				weight[x ][y+1] = weight[x][y]
+						+ (getTileId(x, y) == 0 ? 4 : 1);
+				process(x , y+1, weight);
 			}
 		}
 	}
-
 }
